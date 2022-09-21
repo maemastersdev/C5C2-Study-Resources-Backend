cd36ef91f05ac054a2115dbb2ed10a473e231284
mae masters <maemastersdev@gmail.com>
2022-09-21 17:18:39 +0100

    updating SQL queries

--- tables/createResourcesTable.sql
+++ tables/createResourcesTable.sql
@@ -6,9 +6,9 @@ CREATE TABLE resources (
     resource_name VARCHAR(255) NOT NULL,
     author_name VARCHAR(255),
     url VARCHAR(255) UNIQUE NOT NULL,
-    content_type content_types DEFAULT 'Other',
-    learning_stage INT DEFAULT 'Any',
+    content_type content_types DEFAULT 'other',
+    learning_stage INT DEFAULT 0,
     date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    user_id REFERENCES users(user_id);
+    user_id INT REFERENCES users(user_id),
     review VARCHAR(255)
 );
--- tables/createTableFavourites.sql
+++ tables/createTableFavourites.sql
@@ -1,4 +1,4 @@
 CREATE TABLE favourites (
     user_id INT REFERENCES users(user_id),
-    resource_id INT REFERENCES resources(resource_id);
-)
+    resource_id INT REFERENCES resources(resource_id),
+);
